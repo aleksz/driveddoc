a66a7f30b2e813de549b2513ac875d62b993cc4e
Aleksandr Zhuikov <aleksandr.zhuikov@gmail.com>
2014-10-09 20:45:32 +0300

    #11 using HttpClient 4.0.1 due to conflict with google-http-client

--- src/main/java/com/gmail/at/zhuikov/aleksandr/driveddoc/service/MobileIdService.java
+++ src/main/java/com/gmail/at/zhuikov/aleksandr/driveddoc/service/MobileIdService.java
@@ -12,15 +12,14 @@ import java.util.logging.Logger;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import javax.net.ssl.SSLContext;
 
 import org.apache.commons.compress.utils.IOUtils;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
-import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.DefaultHttpClient;
 
 import com.gmail.at.zhuikov.aleksandr.driveddoc.model.SignatureContainerDescription;
 import com.gmail.at.zhuikov.aleksandr.driveddoc.repository.SignatureContainerDescriptionRepository;
@@ -210,15 +209,16 @@ public class MobileIdService {
 	}
 	
 	private String pullUrl(String url, String msg) {
+
+		HttpClient httpclient = getHttpClient();
 		
 		HttpPost request = new HttpPost(url);
 		request.setHeader("Content-Type", "text/xml; charset=utf-8");
 		request.setHeader("User-Agent", SignedDoc.LIB_NAME + " / " + SignedDoc.LIB_VERSION);
 		request.setHeader("SOAPAction", "");
-		request.setEntity(new StringEntity(msg, "UTF-8"));
 		
-		HttpClient httpclient = getHttpClient();
 		try {
+			request.setEntity(new StringEntity(msg, "UTF-8"));
 			return new String(IOUtils.toByteArray(httpclient.execute(request).getEntity().getContent()));
 		} catch (IOException e) {
 			throw new RuntimeException(e);
@@ -226,25 +226,22 @@ public class MobileIdService {
 	}
 
 	protected HttpClient getHttpClient() {
-		return HttpClients.custom()
-				.setSSLSocketFactory(new SSLConnectionSocketFactory(configureSSLContext()))
-				.build();
-	}
-
-	protected SSLContext configureSSLContext() {
+		SignatureContainerDescription description = signatureContainerDescriptionRepository.get("master");
+		
+		HttpClient client = new DefaultHttpClient();
+		KeyStore keyStore = loadKeyStore(description);
+		
 		try {
-			return SSLContexts.custom()
-					.loadKeyMaterial(loadKeyStore(),  "097850N7".toCharArray())
-					.build();
-		} catch (KeyManagementException | NoSuchAlgorithmException
-				| KeyStoreException | UnrecoverableKeyException e) {
+			Scheme scheme = new Scheme("https",	new SSLSocketFactory(keyStore, description.getPassword()), 443);
+			client.getConnectionManager().getSchemeRegistry().register(scheme);
+		} catch (KeyManagementException | UnrecoverableKeyException
+				| NoSuchAlgorithmException | KeyStoreException e) {
 			throw new RuntimeException(e);
 		}
+	    return client;
 	}
 
-	protected KeyStore loadKeyStore() {
-		
-		SignatureContainerDescription description = signatureContainerDescriptionRepository.get("master");
+	protected KeyStore loadKeyStore(SignatureContainerDescription description) {
 		
 		try {
 			KeyStore trustStore = KeyStore.getInstance("pkcs12");
